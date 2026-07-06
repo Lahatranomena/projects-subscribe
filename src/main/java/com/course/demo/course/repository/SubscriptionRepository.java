3eb62672cb692e211f8ce191d840f1ba86b2f354
Lahatranomena <hei.lahatra.3@gmail.com>
2026-07-06 09:47:59 +0300

    feat(format): add format to Repository

--- src/main/java/com/course/demo/course/repository/SubscriptionRepository.java
+++ src/main/java/com/course/demo/course/repository/SubscriptionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
 
-    Optional<Subscription> findByUserIdAndCourseId(UUID userId, UUID courseId);
+  Optional<Subscription> findByUserIdAndCourseId(UUID userId, UUID courseId);
 }
