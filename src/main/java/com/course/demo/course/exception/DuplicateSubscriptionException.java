30bd14174531e23cd927dfe813e3fafc48890671
Valisoa Tolotriniaina <167887165+valisoa01@users.noreply.github.com>
2026-07-06 09:15:24 +0300

    Subscription controller (#5)
    * feat/add exception
    
    * fix/format

---
+++ src/main/java/com/course/demo/course/exception/DuplicateSubscriptionException.java
@@ -0,0 +1,11 @@
+package com.course.demo.course.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DuplicateSubscriptionException extends RuntimeException {
+  public DuplicateSubscriptionException(String message) {
+    super(message);
+  }
+}
